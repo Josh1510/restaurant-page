d3ec938b356eaadedfd53fbea1c87d2e3ee75a07
josh1510 <joshmoon.1510@gmail.com>
2021-07-09 16:39:16 +1000

    na

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,23 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+                options: {
+                    outputPath: 'images',
+                },
+            },
+        ],
+    },
 };
