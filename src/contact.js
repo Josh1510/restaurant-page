1ed63b9e0c2e960c24b838ba4223100f68b9af78
josh1510 <joshmoon.1510@gmail.com>
2021-07-09 17:32:23 +1000

    loading in map for contact page

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,23 @@
 const loadContact = () => {
+    console.log('contact loading');
+    let mainContent = document.getElementById('content');
+
+    // reset page
+    mainContent.innerHTML = '';
+
+    mainContent.innerHTML = `
+    <div>We're so close! Come and say hello.</div>
+    <iframe
+        src="https://maps.google.com/maps?q=home&t=&z=19&ie=UTF8&iwloc=&output=embed"
+        width="600"
+        height="450"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0"
+    ></iframe>`;
+
     console.log('contact loaded~');
 };
 
