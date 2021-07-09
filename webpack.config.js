82017701ceef011ae2c70fed87ab270008e6bc96
josh1510 <joshmoon.1510@gmail.com>
2021-07-09 17:33:23 +1000

    Image import rules

--- webpack.config.js
+++ webpack.config.js
@@ -18,19 +18,8 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-                options: {
-                    outputPath: 'images',
-                },
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
